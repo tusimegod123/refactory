ae11036e033f314dab5d9ed6aa16088fdebad39d
tusimegod123 <43960999+tusimegod123@users.noreply.github.com>
2020-07-15 18:23:15 +0300

    Initial Commit for Godwin

---
+++ test/newcodelaboneTest.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+const chai = require('chai');
+const assertArrays = require('chai-arrays');
+const computeGeometries = require('../final.js');
+// eslint-disable-next-line prefer-destructuring
+const expect = chai.expect;
+
+chai.use(assertArrays);
+
+describe('isAnArray', () => {
+    it('Should return an Array', () => {
+        expect([computeGeometries]).to.be.array();
+    });
+});
